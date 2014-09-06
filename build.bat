5470e8309eadb3544b73493cea6ad5a91a49c12c
Dane Springmeyer <dane@mapbox.com>
2014-09-06 21:35:42 +0000

    only copy files that are new

--- build.bat
+++ build.bat
@@ -44,31 +44,31 @@ IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 
 :: includes
-xcopy /q %DEPSDIR%\harfbuzz-build\harfbuzz\hb-version.h %MAPNIK_SDK%\includes\harfbuzz\ /Y
+xcopy /q /d %DEPSDIR%\harfbuzz-build\harfbuzz\hb-version.h %MAPNIK_SDK%\includes\harfbuzz\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\harfbuzz\src\hb.h %MAPNIK_SDK%\includes\harfbuzz\ /Y
+xcopy /q /d %DEPSDIR%\harfbuzz\src\hb.h %MAPNIK_SDK%\includes\harfbuzz\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\harfbuzz\src\hb-shape-plan.h %MAPNIK_SDK%\includes\harfbuzz\ /Y
+xcopy /q /d %DEPSDIR%\harfbuzz\src\hb-shape-plan.h %MAPNIK_SDK%\includes\harfbuzz\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\harfbuzz\src\hb-shape.h %MAPNIK_SDK%\includes\harfbuzz\ /Y
+xcopy /q /d %DEPSDIR%\harfbuzz\src\hb-shape.h %MAPNIK_SDK%\includes\harfbuzz\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\harfbuzz\src\hb-set.h %MAPNIK_SDK%\includes\harfbuzz\ /Y
+xcopy /q /d %DEPSDIR%\harfbuzz\src\hb-set.h %MAPNIK_SDK%\includes\harfbuzz\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\harfbuzz\src\hb-ft.h %MAPNIK_SDK%\includes\harfbuzz\ /Y
+xcopy /q /d %DEPSDIR%\harfbuzz\src\hb-ft.h %MAPNIK_SDK%\includes\harfbuzz\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\harfbuzz\src\hb-buffer.h %MAPNIK_SDK%\includes\harfbuzz\ /Y
+xcopy /q /d %DEPSDIR%\harfbuzz\src\hb-buffer.h %MAPNIK_SDK%\includes\harfbuzz\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy  /q %DEPSDIR%\harfbuzz\src\hb-unicode.h %MAPNIK_SDK%\includes\harfbuzz\ /Y
+xcopy /q /d %DEPSDIR%\harfbuzz\src\hb-unicode.h %MAPNIK_SDK%\includes\harfbuzz\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy  /q %DEPSDIR%\harfbuzz\src\hb-common.h %MAPNIK_SDK%\includes\harfbuzz\ /Y
+xcopy /q /d %DEPSDIR%\harfbuzz\src\hb-common.h %MAPNIK_SDK%\includes\harfbuzz\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy  /q %DEPSDIR%\harfbuzz\src\hb-blob.h %MAPNIK_SDK%\includes\harfbuzz\ /Y
+xcopy /q /d %DEPSDIR%\harfbuzz\src\hb-blob.h %MAPNIK_SDK%\includes\harfbuzz\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy  /q %DEPSDIR%\harfbuzz\src\hb-font.h %MAPNIK_SDK%\includes\harfbuzz\ /Y
+xcopy /q /d %DEPSDIR%\harfbuzz\src\hb-font.h %MAPNIK_SDK%\includes\harfbuzz\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy  /q %DEPSDIR%\harfbuzz\src\hb-face.h %MAPNIK_SDK%\includes\harfbuzz\ /Y
+xcopy /q /d %DEPSDIR%\harfbuzz\src\hb-face.h %MAPNIK_SDK%\includes\harfbuzz\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy  /q %DEPSDIR%\harfbuzz\src\hb-deprecated.h %MAPNIK_SDK%\includes\harfbuzz\ /Y
+xcopy /q /d %DEPSDIR%\harfbuzz\src\hb-deprecated.h %MAPNIK_SDK%\includes\harfbuzz\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 xcopy /i /d /s /q %DEPSDIR%\boost_1_56_0\boost %MAPNIK_SDK%\includes\boost /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
@@ -78,107 +78,107 @@ xcopy /i /d /s /q %DEPSDIR%\freetype\include %MAPNIK_SDK%\includes\freetype2 /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 xcopy /i /d /s /q %DEPSDIR%\libxml2\include %MAPNIK_SDK%\includes\libxml2 /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\zlib\zlib.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\zlib\zlib.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\zlib\zconf.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\zlib\zconf.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\libpng\png.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\libpng\png.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\libpng\pnglibconf.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\libpng\pnglibconf.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\libpng\pngconf.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\libpng\pngconf.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\jpeg\jpeglib.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\jpeg\jpeglib.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\jpeg\jconfig.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\jpeg\jconfig.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\jpeg\jmorecfg.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\jpeg\jmorecfg.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 xcopy /i /d /s /q %DEPSDIR%\webp\src\webp %MAPNIK_SDK%\includes\webp /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\proj\src\proj_api.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\proj\src\proj_api.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\libtiff\libtiff\tiff.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\libtiff\libtiff\tiff.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\libtiff\libtiff\tiffvers.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\libtiff\libtiff\tiffvers.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\libtiff\libtiff\tiffconf.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\libtiff\libtiff\tiffconf.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\libtiff\libtiff\tiffio.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\libtiff\libtiff\tiffio.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\cairo\cairo-version.h %MAPNIK_SDK%\includes\cairo\ /Y
+xcopy /q /d %DEPSDIR%\cairo\cairo-version.h %MAPNIK_SDK%\includes\cairo\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\cairo\src\cairo-features.h %MAPNIK_SDK%\includes\cairo\ /Y
+xcopy /q /d %DEPSDIR%\cairo\src\cairo-features.h %MAPNIK_SDK%\includes\cairo\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\cairo\src\cairo.h %MAPNIK_SDK%\includes\cairo\ /Y
+xcopy /q /d %DEPSDIR%\cairo\src\cairo.h %MAPNIK_SDK%\includes\cairo\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\cairo\src\cairo-deprecated.h %MAPNIK_SDK%\includes\cairo\ /Y
+xcopy /q /d %DEPSDIR%\cairo\src\cairo-deprecated.h %MAPNIK_SDK%\includes\cairo\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\cairo\src\cairo-svg.h %MAPNIK_SDK%\includes\cairo\ /Y
+xcopy /q /d %DEPSDIR%\cairo\src\cairo-svg.h %MAPNIK_SDK%\includes\cairo\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\cairo\src\cairo-svg-surface-private.h %MAPNIK_SDK%\includes\cairo\ /Y
+xcopy /q /d %DEPSDIR%\cairo\src\cairo-svg-surface-private.h %MAPNIK_SDK%\includes\cairo\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\cairo\src\cairo-pdf.h %MAPNIK_SDK%\includes\cairo\ /Y
+xcopy /q /d %DEPSDIR%\cairo\src\cairo-pdf.h %MAPNIK_SDK%\includes\cairo\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\cairo\src\cairo-ft.h %MAPNIK_SDK%\includes\cairo\ /Y
+xcopy /q /d %DEPSDIR%\cairo\src\cairo-ft.h %MAPNIK_SDK%\includes\cairo\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\cairo\src\cairo-ps.h %MAPNIK_SDK%\includes\cairo\ /Y
+xcopy /q /d %DEPSDIR%\cairo\src\cairo-ps.h %MAPNIK_SDK%\includes\cairo\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 xcopy /i /d /s /q %DEPSDIR%\protobuf\vsprojects\include %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 :: libs
-xcopy /q %DEPSDIR%\harfbuzz-build\harfbuzz.lib %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\harfbuzz-build\harfbuzz.lib %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\freetype\freetype.lib %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\freetype\freetype.lib %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\icu\lib\icuuc.lib %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\icu\lib\icuuc.lib %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\icu\lib\icuin.lib %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\icu\lib\icuin.lib %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\icu\bin\icuuc53.dll %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\icu\bin\icuuc53.dll %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\icu\bin\icudt53.dll %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\icu\bin\icudt53.dll %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\icu\bin\icuin53.dll %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\icu\bin\icuin53.dll %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\libxml2\win32\bin.msvc\libxml2_a.lib %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\libxml2\win32\bin.msvc\libxml2_a.lib %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\libxml2\win32\bin.msvc\libxml2_a_dll.lib %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\libxml2\win32\bin.msvc\libxml2_a_dll.lib %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\libxml2\win32\bin.msvc\libxml2.dll %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\libxml2\win32\bin.msvc\libxml2.dll %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\libxml2\win32\bin.msvc\libxml2.lib %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\libxml2\win32\bin.msvc\libxml2.lib %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\libtiff\libtiff\libtiff.dll %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\libtiff\libtiff\libtiff.dll %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 ::xcopy /i /d /s /q %DEPSDIR%\libtiff\libtiff\libtiff.lib %MAPNIK_SDK%\libs\ /Y
 ::IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\libtiff\libtiff\libtiff_i.lib %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\libtiff\libtiff\libtiff_i.lib %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\zlib\zlib.lib %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\zlib\zlib.lib %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\proj\src\proj.lib %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\proj\src\proj.lib %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\webp\output\release-dynamic\x86\lib\libwebp_dll.lib %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\webp\output\release-dynamic\x86\lib\libwebp_dll.lib %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\webp\output\release-dynamic\x86\bin\libwebp.dll %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\webp\output\release-dynamic\x86\bin\libwebp.dll %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\libpng\projects\vstudio\Release\libpng16.lib %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\libpng\projects\vstudio\Release\libpng16.lib %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\libpng\projects\vstudio\Release\libpng16.dll %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\libpng\projects\vstudio\Release\libpng16.dll %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\jpeg\libjpeg.lib %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\jpeg\libjpeg.lib %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\cairo\src\release\cairo-static.lib %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\cairo\src\release\cairo-static.lib %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\cairo\src\release\cairo.lib %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\cairo\src\release\cairo.lib %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\cairo\src\release\cairo.dll %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\cairo\src\release\cairo.dll %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 xcopy /i /d /s /q %DEPSDIR%\boost_1_56_0\stage\lib\* %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\protobuf\vsprojects\Release\libprotobuf-lite.lib %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\protobuf\vsprojects\Release\libprotobuf-lite.lib %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 :: data
@@ -188,81 +188,81 @@ xcopy /i /d /s /q %DEPSDIR%\gdal\data %MAPNIK_SDK%\share\gdal
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 :: bin
-xcopy /q %DEPSDIR%\protobuf\vsprojects\Release\protoc.exe %MAPNIK_SDK%\bin /Y
+xcopy /q /d %DEPSDIR%\protobuf\vsprojects\Release\protoc.exe %MAPNIK_SDK%\bin /Y
 xcopy /q mapnik-config.bat %MAPNIK_SDK%\bin /Y
 
 :: headers for plugins
-xcopy /q %DEPSDIR%\postgresql\src\interfaces\libpq\libpq-fe.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\postgresql\src\interfaces\libpq\libpq-fe.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\postgresql\src\include\postgres_ext.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\postgresql\src\include\postgres_ext.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\postgresql\src\include\pg_config_ext.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\postgresql\src\include\pg_config_ext.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\sqlite\sqlite3.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\sqlite\sqlite3.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-::xcopy /q %DEPSDIR%\gdal\gcore\*h %MAPNIK_SDK%\includes\gdal\ /Y
+::xcopy /q /d %DEPSDIR%\gdal\gcore\*h %MAPNIK_SDK%\includes\gdal\ /Y
 ::IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\gdal\ogr\ogr_feature.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\gdal\ogr\ogr_feature.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\gdal\ogr\ogr_spatialref.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\gdal\ogr\ogr_spatialref.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\gdal\ogr\ogr_geometry.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\gdal\ogr\ogr_geometry.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\gdal\ogr\ogr_core.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\gdal\ogr\ogr_core.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\gdal\ogr\ogr_featurestyle.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\gdal\ogr\ogr_featurestyle.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\gdal\ogr\ogrsf_frmts\ogrsf_frmts.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\gdal\ogr\ogrsf_frmts\ogrsf_frmts.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\gdal\ogr\ogr_srs_api.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\gdal\ogr\ogr_srs_api.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\gdal\gcore\gdal_priv.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\gdal\gcore\gdal_priv.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\gdal\gcore\gdal_frmts.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\gdal\gcore\gdal_frmts.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\gdal\gcore\gdal.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\gdal\gcore\gdal.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\gdal\gcore\gdal_version.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\gdal\gcore\gdal_version.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\gdal\port\cpl_minixml.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\gdal\port\cpl_minixml.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\gdal\port\cpl_atomic_ops.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\gdal\port\cpl_atomic_ops.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\gdal\port\cpl_string.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\gdal\port\cpl_string.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\gdal\port\cpl_conv.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\gdal\port\cpl_conv.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\gdal\port\cpl_vsi.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\gdal\port\cpl_vsi.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\gdal\port\cpl_virtualmem.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\gdal\port\cpl_virtualmem.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\gdal\port\cpl_error.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\gdal\port\cpl_error.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\gdal\port\cpl_progress.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\gdal\port\cpl_progress.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\gdal\port\cpl_port.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\gdal\port\cpl_port.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\gdal\port\cpl_config.h %MAPNIK_SDK%\includes\ /Y
+xcopy /q /d %DEPSDIR%\gdal\port\cpl_config.h %MAPNIK_SDK%\includes\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 :: libs for plugins
-xcopy /q %DEPSDIR%\postgresql\src\interfaces\libpq\Release\libpq.lib %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\postgresql\src\interfaces\libpq\Release\libpq.lib %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\postgresql\src\interfaces\libpq\Release\libpq.dll %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\postgresql\src\interfaces\libpq\Release\libpq.dll %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\sqlite\sqlite3.lib %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\sqlite\sqlite3.lib %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\gdal\gdal_i.lib %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\gdal\gdal_i.lib %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 :: NOTE: impossible to statically link gdal due to:
 :: http://stackoverflow.com/questions/4596212/c-odbc-refuses-to-statically-link-to-libcmt-lib-under-vs2010
-::xcopy /q %DEPSDIR%\gdal\gdal.lib %MAPNIK_SDK%\libs\ /Y
+::xcopy /q /d %DEPSDIR%\gdal\gdal.lib %MAPNIK_SDK%\libs\ /Y
 ::IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\gdal\gdal111.dll %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\gdal\gdal111.dll %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\expat\win32\bin\Release\libexpat.lib %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\expat\win32\bin\Release\libexpat.lib %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-xcopy /q %DEPSDIR%\expat\win32\bin\Release\libexpat.dll %MAPNIK_SDK%\libs\ /Y
+xcopy /q /d %DEPSDIR%\expat\win32\bin\Release\libexpat.dll %MAPNIK_SDK%\libs\ /Y
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 :: detect trouble with mimatched linking
